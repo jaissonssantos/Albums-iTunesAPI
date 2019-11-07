ae40c5e162f9b229b5d68c629dc672219f668e30
jaissonssantos <jaissonssantos@gmail.com>
2019-11-07 11:43:24 -0500

    update styles and pages

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,27 +1,94 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import { FaSearch } from 'react-icons/fa';
+import { FaSearch, FaSpinner } from 'react-icons/fa';
 import logo from '../../assets/logo.svg';
-import { Container, Form, SubmitButton } from './styles';
+import { Container, Form, SubmitButton, List } from './styles';
 
-export default function Main() {
-    return (
-        <Container>
-            <img src={logo} alt="iMusic" />
+import api from '../../services/api';
 
-            <h1>Música para todos.</h1>
-            <h2>
-                Milhões de artistas à sua escolha. Procure por seu artista
-                preferido :)
-            </h2>
+export default class Main extends Component {
+    state = {
+        artists: '',
+        results: [],
+        loading: false,
+    };
 
-            <Form onSubmit={() => {}}>
-                <input type="input" placeholder="Qual o nome do seu artista?" />
+    handlerSearchArtists = e => {
+        this.setState({ artists: e.target.value });
+    };
 
-                <SubmitButton disabled>
-                    <FaSearchAlt color="#FFF" size={14} />
-                </SubmitButton>
-            </Form>
-        </Container>
-    );
+    handlerSubmitForm = async e => {
+        e.preventDefault();
+
+        this.setState({ loading: true });
+
+        const { artists, results } = this.state;
+
+        const response = await api.get('/search', {
+            params: {
+                term: artists,
+                limit: 10,
+            },
+        });
+
+        this.setState({
+            results: [...response.data.results],
+            artists: '',
+            loading: false,
+        });
+
+        // console.log(response.data);
+        console.log(this.state.results);
+    };
+
+    render() {
+        const { artists, loading, results } = this.state;
+
+        return (
+            <Container>
+                <img src={logo} alt="iMusic" />
+
+                <h1>Música para todos.</h1>
+                <h2>
+                    Milhões de artistas à sua escolha. Procure por seu artista
+                    preferido :)
+                </h2>
+
+                <Form onSubmit={this.handlerSubmitForm}>
+                    <input
+                        type="input"
+                        placeholder="Qual o nome do seu artista?"
+                        value={artists}
+                        onChange={this.handlerSearchArtists}
+                    />
+
+                    <SubmitButton loading={loading}>
+                        {loading ? (
+                            <FaSpinner color="#FFF" size={16} />
+                        ) : (
+                            <FaSearch color="#FFF" size={16} />
+                        )}
+                    </SubmitButton>
+                </Form>
+
+                <List>
+                    {results.map(result => (
+                        <li key={String(result.artistId)}>
+                            <a href="#">
+                                <img
+                                    src={result.artworkUrl100}
+                                    alt={result.artistName}
+                                />
+                                <div>
+                                    <strong>{result.artistName}</strong>
+                                    <p>{result.collectionName}</p>
+                                    <p>{result.primaryGenreName}</p>
+                                </div>
+                            </a>
+                        </li>
+                    ))}
+                </List>
+            </Container>
+        );
+    }
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
 export const Container = styled.div`
     max-width: 620px;
@@ -20,7 +20,6 @@ export const Container = styled.div`
         flex-direction: row;
         align-items: center;
         justify-content: center;
-        text-align: center;
         font-weight: 600;
         letter-spacing: -0.04em;
         line-height: 1.15;
@@ -32,9 +31,20 @@ export const Container = styled.div`
         flex-direction: row;
         align-items: center;
         justify-content: center;
-        font-weight: 300;
         text-align: center;
+        font-weight: 300;
         color: #32323d !important;
+    }
+
+    @media (max-width: 480px) {
+        margin: 60px 15px !important;
+
+        h1 {
+            font-size: 30px !important;
+        }
+        h2 {
+            font-size: 16px !important;
+        }
     }
 `;
 
@@ -51,6 +61,116 @@ export const Form = styled.form`
         font-size: 16px;
         font-weight: 300;
     }
+
+    @media (max-width: 480px) {
+        flex-direction: column !important;
+
+        input {
+            flex: 1;
+        }
+    }
 `;
 
-export const SubmitButton = styled.button``;
+const rotateSpinner = keyframes`
+    from{
+        transform: rotate(0deg);
+    }
+
+    to{
+        transform: rotate(360deg);
+    }
+`;
+
+export const SubmitButton = styled.button.attrs(props => ({
+    type: 'submit',
+    disabled: props.loading,
+}))`
+    background-color: #ef5466;
+    border: 0;
+    padding: 0 20px;
+    margin-left: 15px;
+    border-radius: 4px;
+    display: flex;
+    justify-items: center;
+    align-items: center;
+    cursor: pointer;
+
+    &[disabled] {
+        cursor: not-allowed;
+        opacity: 0.6;
+    }
+
+    ${props =>
+        props.loading &&
+        css`
+            svg {
+                animation: ${rotateSpinner} 2s linear infinite;
+            }
+        `}
+
+    @media (max-width: 480px) {
+        flex: 1;
+        flex-direction: column !important;
+        margin: 15px 0 0;
+        padding: 15px;
+    }
+`;
+
+export const List = styled.ul`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 20px;
+    list-style: none;
+    margin-top: 25px;
+
+    li {
+        display: flex;
+        flex: 1;
+        flex-direction: column;
+    }
+
+    a {
+        display: flex;
+        flex: 1;
+        flex-direction: column;
+        align-items: stretch;
+        align-content: stretch;
+        align-items: center;
+        color: #ef5466;
+        text-decoration: none;
+        padding: 18px 15px;
+        border-radius: 4px;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+
+        &:hover {
+            box-shadow: 0 0 30px rgba(0, 0, 0, 0.18);
+        }
+    }
+
+    img {
+        align-self: center;
+        width: 100px;
+        height: 100px;
+        margin-bottom: 15px;
+        border-radius: 50%;
+        border: 2px solid #eee;
+    }
+
+    div {
+        display: flex;
+        align-items: center;
+        overflow: hidden;
+        margin-top: auto;
+        flex-direction: column;
+
+        strong {
+            flex: 1;
+        }
+
+        p {
+            flex: 1;
+            font-size: 12px;
+            color: #333;
+        }
+    }
+`;
